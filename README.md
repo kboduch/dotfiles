96bf7954d63eea45d361726055296d24a51098f8
Krzysztof Boduch <boduch.krzysztof@gmail.com>
2024-04-27 09:37:36 +0200

    almost there

---
+++ README.md
@@ -0,0 +1,9 @@
+1. Install Xcode develper tools 
+2. Install Homebrew https://brew.sh
+3. Install and configure 1Password
+    1. brew install --cask 1password
+    2. brew install --cask 1password-cli
+    3. Configure 1Password
+    4. Configure 1Password SSH & Git
+4. Install chezmoi with `brew install chezmoi`
+5. Run `chezmoi init kboduch`
---
+++ home/dot_hammerspoon/init.lua
@@ -0,0 +1,11 @@
+hs.loadSpoon("WindowHalfsAndThirds")
+
+spoon.WindowHalfsAndThirds:bindHotkeys( {
+    left_half    = { {"cmd",       "alt"}, "Left" },
+    right_half   = { {"cmd",       "alt"}, "Right" },
+    max_toggle   = { {"cmd",       "alt"}, "Up" },
+    top_left     = { {"cmd",       "alt"}, "1" },
+    top_right    = { {"cmd",       "alt"}, "2" },
+    bottom_left  = { {"cmd",       "alt"}, "3" },
+    bottom_right = { {"cmd",       "alt"}, "4" },
+ })
