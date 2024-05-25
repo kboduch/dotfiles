797595f1a68463d0393a736c01dff3a24a27b18a
Krzysztof Boduch <boduch.krzysztof@gmail.com>
2024-05-25 02:05:38 +0200

    a little automation - ask to update once every 24h

---
+++ home/dot_kboduch/executable_execute_chezmoi_update_once_a_day.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LAST_RUN_FILE="$HOME/.last_chezmoi_update_execution_timestamp.txt"
+CURRENT_TIMESTAMP=$(date +%s)
+ONE_DAY_SECONDS=$((24 * 60 * 60))
+
+if [ -f "$LAST_RUN_FILE" ]; then
+  LAST_RUN_TIMESTAMP=$(cat "$LAST_RUN_FILE")
+  TIME_DIFF=$((CURRENT_TIMESTAMP - LAST_RUN_TIMESTAMP))
+  if [ $TIME_DIFF -lt $ONE_DAY_SECONDS ]; then
+    exit 0
+  fi
+fi
+
+read -p "The last chezmoi update was over 24 hours ago. Would you like to run it now (y/n)? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  source ~/.kboduch/common.lib.sh
+  run_command "chezmoi update"
+  echo "$CURRENT_TIMESTAMP" > "$LAST_RUN_FILE"
+fi
